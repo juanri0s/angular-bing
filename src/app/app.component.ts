d073826d17a19a658063504a01689eee99f81a8c
Juan Rios <juan.rios.28@gmail.com>
2019-09-26 22:27:14 -0400

    Added polyline support.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string = 'NG Bing';
+  title: string = 'angular-bing';
   height: number;
 
   ngOnInit(): void {
--- src/app/bing-map/bing-map.component.ts
+++ src/app/bing-map/bing-map.component.ts
@@ -1,6 +1,6 @@
 // tslint:disable-next-line:no-reference
 /// <reference path="../../../node_modules/bingmaps/types/MicrosoftMaps/Microsoft.Maps.d.ts" />
-import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {BingMapService} from '../bing-map.service';
@@ -9,7 +9,8 @@ import {BingMapService} from '../bing-map.service';
   selector: 'app-bing-map',
   templateUrl: './bing-map.component.html',
   styleUrls: ['./bing-map.component.css'],
-  providers: [BingMapService]
+  providers: [BingMapService],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BingMapComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() height: number;
@@ -17,11 +18,13 @@ export class BingMapComponent implements OnInit, AfterViewInit, OnDestroy {
   private CITIES_PATH: string = 'assets/json/cities.json';
   private jsonReq: any;
   private mapReq: any;
+  private cities: Array<City> = [];
+  private pins: Array<any> = [];
 
   constructor(private http: HttpClient, private bing: BingMapService) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getMap();
   }
 
@@ -46,18 +49,20 @@ export class BingMapComponent implements OnInit, AfterViewInit, OnDestroy {
     return this.http.get(this.CITIES_PATH);
   }
 
-  generateMap() {
+  generateMap(): void {
     // only subscribe if we don't have the map initialized yet
     if (!this.bing.map) {
       this.mapReq = this.bing.isMapSetup.subscribe(map => {
         if (map) {
           this.jsonReq = this.getJSON().subscribe((cities: Array<City>) => {
+            this.cities = cities;
             this.createPins(cities);
           });
         }
       });
     } else {
       this.jsonReq = this.getJSON().subscribe((cities: Array<City>) => {
+        this.cities = cities;
         this.createPins(cities);
       });
     }
@@ -67,27 +72,73 @@ export class BingMapComponent implements OnInit, AfterViewInit, OnDestroy {
     // make sure there aren't already pins on the map
     if (this.bing.map.entities.getLength() === 0) {
       for (const city of Object.values(cities)) {
-        const center = new Microsoft.Maps.Location(city.latitude, city.longitude);
-        const pin = new Microsoft.Maps.Pushpin(center, {
+        const coord = new Microsoft.Maps.Location(city.latitude, city.longitude);
+        const pin = new Microsoft.Maps.Pushpin(coord, {
           title: city.state,
         });
 
-        this.setPins(pin);
+        this.pins.push(pin);
       }
+
+      this.setPins(this.pins);
     }
   }
 
-  setPins(pin: any) {
+  setPins(pin: any): void {
     this.bing.map.entities.push(pin);
   }
 
-  clearPins() {
+  clearPins(): void {
     for (let i = this.bing.map.entities.getLength() - 1; i >= 0; i--) {
       const pin = this.bing.map.entities.get(i);
+
+      // If there are polylines we need to clear them too
+      if (pin instanceof Microsoft.Maps.Polyline) {
+        this.bing.map.entities.removeAt(i);
+      }
+
       if (pin instanceof Microsoft.Maps.Pushpin) {
         this.bing.map.entities.removeAt(i);
       }
     }
+  }
+
+  connectPins(): void {
+    if (this.bing.map.entities.getLength() !== 0) {
+      let coordA: any;
+      let coordB: any;
+      const numStates = 50;
+      // tslint:disable-next-line:forin
+      for (const key in this.cities) {
+        if (Number(key) === numStates - 1) {
+          return;
+        }
+
+        const city = this.cities[key];
+        const nextCity = this.cities[Number(key) + 1];
+
+        // coordA will be the first point, coordB will be the 2nd point, and then
+        // coordB will become coordA for the next iteration
+        if (!coordA && !coordB) {
+          coordA = new Microsoft.Maps.Location(city.latitude, city.longitude);
+          coordB = new Microsoft.Maps.Location(nextCity.latitude, nextCity.longitude);
+        }
+
+        coordA = coordB;
+        coordB = new Microsoft.Maps.Location(nextCity.latitude, nextCity.longitude);
+
+        const coords = [coordA, coordB];
+
+        const line = new Microsoft.Maps.Polyline(coords, {
+          strokeColor: 'red',
+          strokeThickness: 3,
+          strokeDashArray: [4, 4]
+        });
+
+        this.bing.map.entities.push(line);
+      }
+    }
+
   }
 
   ngOnDestroy(): void {
